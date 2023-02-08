d420dde61aa5554c4bf96063e7d12720340fd342
Alan S <alnjsantos@hotmail.com>
2023-02-08 18:40:30 +0000

    add swagger

--- src/main/java/com/apicliente/ApiApplication.java
+++ src/main/java/com/apicliente/ApiApplication.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ApiApplication {
 
 	public static void main(String[] args) {
